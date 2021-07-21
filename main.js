80795a2868ccbfcaeeedafe0be1295c1f0c03a0b
VC-PopcornOlympics <87766416+VC-PopcornOlympics@users.noreply.github.com>
2021-07-21 19:14:32 +0530

    Beta Version 1 uploaded to repositry

---
+++ main.js
@@ -0,0 +1,5 @@
+window.alert("You are using a beta version of this website, If you report any bugs , kindly report to sender of this link")
+function notavlaible(){
+    window.alert("This function is not currently realesed in Beta Series, To get the function wait for an update (To check update, check website regularly)(last updated 27/7/21) and check that this feature got enabled or not, Every feature will be realesed in official non-beta uppdate, Thanks for using Beta!");
+    window.alert("Use Latest version of your browser to double check that this is not currently realesed");
+}
